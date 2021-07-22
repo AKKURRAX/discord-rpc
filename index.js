666951e967d451f00b0feb55cc2a29e2f1cdb981
AKKURRAX <73422934+AKKURRAX@users.noreply.github.com>
2021-07-23 01:43:13 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,41 @@
+const rpc = require('discord-rpc');
+
+const config = {
+	appId: "850660876315000852", 
+
+	details: "En ligne 🟢", 
+	largeImageKeyName: "large-image-key", 
+	largeImageText: "XXXXXXXX",
+	smallImageKeyName: "small-image-key", 
+	smallImageText: "XXXXXXXX", 
+
+	buttonOneName: "Invite bot", 
+	buttonOneUrl: "https://discord.com/api/oauth2/authorize?client_id=850660876315000852&permissions=8&scope=bot"
+};
+
+rpc.register(config.appId);
+const client = new rpc.Client({transport: 'ipc'});
+
+client.on('ready', () => {
+	console.log('RPC Started');
+	client.setActivity({
+		details: config.details,
+		largeImageKey: config.largeImageKeyName,
+		largeImageText: config.largeImageText,
+		smallImageKey: config.smallImageKeyName,
+		smallImageText: config.smallImageText,
+		buttons: [
+			{
+				label: config.buttonOneName,
+				url: config.buttonOneUrl
+			},
+			{
+				label: config.buttonTwoName,
+				url: config.buttonTwoUrl
+			}
+		]
+	});
+});
+
+const clientId = config.appId
+client.login({ clientId });
